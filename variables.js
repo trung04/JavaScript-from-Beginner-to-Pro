f52183f02b114aba8318b6d79bdc66091ab4f118
Nguyen Thanh Trung <trunghy2k4111@gmail.com>
2024-07-13 12:22:57 +0700

    submit theory

---
+++ variables.js
@@ -0,0 +1,15 @@
+let cartQuantity=0;
+//5a,b
+let name='Trung';
+console.log(`My name is : ${name}`);
+//5c
+let cost=5+2*3+9;
+console.log(`Cost of  food  : $${cost}`);
+const percentTax=10;
+let tax=cost*percentTax/100;
+console.log(`Cost after Tax : $${tax}`);
+console.log(`Tax (${tax}%) : $${tax}`)
+let totalCost=cost+tax;
+console.log(`Total cost : $${totalCost}`);
+
+let calculation='';
